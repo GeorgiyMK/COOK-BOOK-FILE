63ea31847d045d4a20449f32adde00fb17995ed1
GeorgiyMK <georgiymk82@gmail.com>
2024-11-23 15:25:56 +0300

    task 1

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-def take_recipes(file_name):
-    recipes = {}
-    with open(file_name,'r', encoding = 'utf-8') as f:
-        key = f.readline()
-        recipes[key] = []
-        items_count = f.readline()
-        for line_number in range(items_count):
-            item_line = f.readline()
-            sublines = item_line.split('|')
-            for subline in sublines:
+def read_cookbook(file_name):
+    cook_book = {}
+    with open(file_name, encoding='utf-8') as file:
+        while True:
+            dish_name = file.readline().strip()  # Считываем название блюда
+            if not dish_name:
+                break  # Если строка пустая, выходим из цикла
+            ingredient_count = int(file.readline().strip())  # Считываем количество ингредиентов
+            ingredients = []
+            for _ in range(ingredient_count):
+                ingredient_data = file.readline().strip().split(' | ')
+                ingredients.append({
+                    'ingredient_name': ingredient_data[0],
+                    'quantity': int(ingredient_data[1]),
+                    'measure': ingredient_data[2]
+                })
+            cook_book[dish_name] = ingredients
+            file.readline()  # Пропускаем пустую строку между блюдами
+    return cook_book
 
-        f.readline
-
-take_recipes('recipes.txt')
-
-
+# Использование функции
+file_name = 'recipes.txt'  # Имя вашего файла
+cook_book = read_cookbook(file_name)
+print(cook_book)
